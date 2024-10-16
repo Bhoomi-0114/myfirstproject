56c11a8bb7137ef868765d9d57dcd5c6bb322381
Bhoomi-0114 <barotiabhoomi@gmail.com>
2024-10-16 13:04:39 +0530

    Add files via upload

--- sortedarray.cpp
+++ sortedarray.cpp
@@ -10,3 +10,22 @@ return true;
     }
  }
 } 
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    if (isSorted(n, arr)) {
+        cout << "The array is sorted." << endl;
+    } else {
+        cout << "The array is not sorted." << endl;
+    }
+
+    return 0;
+}
